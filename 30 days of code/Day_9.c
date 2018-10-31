e3620e44bf36cc0cd5ad7844626b2836c5e1f260
Addela Deekshith Reddy <37297557+DeekshithReddyAddela@users.noreply.github.com>
2018-11-01 02:54:01 +0530

    Update Day_9.c

--- 30 days of code/Day_9.c
+++ 30 days of code/Day_9.c
@@ -1,20 +1,13 @@
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
+int factorial(int n){
+        if(n<=1)
+            return 1;
+        else
+            return n*factorial(n-1);
+    }
 
-int factorial(int n) {
-    // Complete this function
-    if(n==0||n==1)return 1;
-    else return n*factorial(n-1);
-}
 int main() {
-    int n; 
-    scanf("%i", &n);
-    int result = factorial(n);
-    printf("%d\n", result);
+    int n;
+    cin >> n;
+    cout << factorial(n);
     return 0;
 }
